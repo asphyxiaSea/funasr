dca62ec9eca7678113434518800fdfcec7ad9368
GUOZEHN <guozhen950716@gmail.com>
2026-03-02 17:07:30 +0800

    模拟向量网络传输流程

--- text.py
+++ text.py
@@ -1,53 +1,38 @@
-# from funasr import AutoModel
-# import torch.nn.functional as F
-
-# model = AutoModel(model="models/SPKmodels/cam++")
-
-# # 1. Generate speaker1 embedding
-# speaker1_emb1 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_a_cn_16k.wav")[0]["spk_embedding"]
-# speaker1_emb2 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_b_cn_16k.wav")[0]["spk_embedding"]
-# speaker2_emb1 = model.generate(input="models/SPKmodels/cam++/examples/speaker2_a_cn_16k.wav")[0]["spk_embedding"]
-
-# # 相似度
-# score = F.cosine_similarity(speaker1_emb1, speaker2_emb1)
-
-# print("相似度:", score.item())
-
-# model = AutoModel(model="models/VADmodels")
-
-
-
 from funasr import AutoModel
+import torch.nn.functional as F
+import numpy as np
+import torch
+import base64
 
-wav_file = "models/VADmodels/speech_fsmn_vad_zh/example/vad_example.wav"
+model = AutoModel(model="models/SPKmodels/cam++")
 
-model = AutoModel(model="models/VADmodels/speech_fsmn_vad_zh")
+# 1. Generate speaker1 embedding
+speaker1_emb1 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_a_cn_16k.wav")[0]["spk_embedding"]
+speaker1_emb2 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_b_cn_16k.wav")[0]["spk_embedding"]
 
-res = model.generate(input=wav_file)
-print(res)
+speaker1_emb1 = F.normalize(speaker1_emb1, p=2, dim=1)
+speaker1_emb2 = F.normalize(speaker1_emb2, p=2, dim=1)
+
+# 将向量转换为二进制数据并转换为Base64字符串
+vector1  = speaker1_emb1.detach().cpu().numpy().astype(np.float32)
+base64_str1 = base64.b64encode(vector1.tobytes()).decode("utf-8")
+
+# 将Base64字符串解码回二进制数据，并转换为原始向量
+base64_str1 = base64.b64decode(base64_str1)
+vector1  = np.frombuffer(base64_str1, dtype=np.float32)
+vector1  = torch.tensor(vector1).unsqueeze(0)
 
 
-import soundfile
-import os
 
-speech, sample_rate = soundfile.read(wav_file)
+vector2 = speaker1_emb2.detach().cpu().numpy().astype(np.float32)
+binary2 = vector2.tobytes()
+base64_str2 = base64.b64encode(binary2).decode("utf-8")
 
-chunk_size = 200  # ms
-chunk_stride = int(chunk_size * sample_rate / 1000)
+base64_str2 = base64.b64decode(base64_str2)
+vector2 = np.frombuffer(base64_str2, dtype=np.float32)
+vector2 = torch.tensor(vector2).unsqueeze(0)
 
-cache = {}
 
-total_chunk_num = int(len((speech) - 1) / chunk_stride + 1)
-for i in range(total_chunk_num):
-    speech_chunk = speech[i * chunk_stride : (i + 1) * chunk_stride]
-    is_final = i == total_chunk_num - 1
-    res = model.generate(
-        input=speech_chunk,
-        cache=cache,
-        is_final=is_final,
-        chunk_size=chunk_size,
-        disable_pbar=True,
-    )
-    # print(res)
-    if len(res[0]["value"]):
-        print(res)
+# 相似度
+score = F.cosine_similarity(vector1, vector2, dim=1)
+print("相似度:", score.item())
