78eefb286101a1582c52bd0c494e0c39d129c6a5
GUOZEHN <guozhen950716@gmail.com>
2026-03-05 15:36:16 +0800

    测试文件

--- text.py
+++ text.py
@@ -7,21 +7,21 @@ import base64
 model = AutoModel(model="models/SPKmodels/cam++")
 
 # 1. Generate speaker1 embedding
-speaker1_emb1 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_a_cn_16k.wav")[0]["spk_embedding"]
+# speaker1_emb1 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_a_cn_16k.wav")[0]["spk_embedding"]
 
-speaker1_emb1 = F.normalize(speaker1_emb1, p=2, dim=1)
+# speaker1_emb1 = F.normalize(speaker1_emb1, p=2, dim=1)
 
 # 将向量转换为二进制数据并转换为Base64字符串
-vector1  = speaker1_emb1.detach().cpu().numpy().astype(np.float32)
-base64_str1 = base64.b64encode(vector1.tobytes()).decode("utf-8")
+# vector1  = speaker1_emb1.detach().cpu().numpy().astype(np.float32)
+# base64_str1 = base64.b64encode(vector1.tobytes()).decode("utf-8")
 
 
 
-
+base64_str1 = "BASE64_STRING_HERE"  # 替换为实际的Base64字符串
 # # 将Base64字符串解码回二进制数据，并转换为原始向量
-# base64_str1 = base64.b64decode(base64_str1)
-# vector1  = np.frombuffer(base64_str1, dtype=np.float32)
-# vector1  = torch.tensor(vector1).unsqueeze(0)
+base64_str1 = base64.b64decode(base64_str1)
+vector1  = np.frombuffer(base64_str1, dtype=np.float32)
+vector1  = torch.tensor(vector1).unsqueeze(0)
 
 
 # speaker1_emb2 = model.generate(input="models/SPKmodels/cam++/examples/speaker1_b_cn_16k.wav")[0]["spk_embedding"]
